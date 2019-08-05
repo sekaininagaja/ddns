c198b2a8030f0114c4f1591b18d2ee809ee58bc9
sekaininagaja <sekaininagaja@gmail.com>
2019-08-05 18:06:27 +0900

    일본 발류도메인에 다이나믹 도메인 등록

--- get_ip.py
+++ get_ip.py
@@ -11,6 +11,11 @@ def main():
             ip = f.readline()
             if ip != content:
                 f.write(content)
+                ip_put_url = 'http://dyn.value-domain.com/cgi-bin/dyn.fcg?d=%s&p=%s&h=%s&i=%s' % (
+                settings.DOMAIN, settings.PASSWORD, settings.HOST, content)
+                print(ip_put_url)
+                response2 = urllib.request.urlopen(ip_put_url, timeout=5)
+                print(response2)
     except urllib.error as e:
         print(type(e))
 
