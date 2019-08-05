c7b32a4c99c9397efe949cc0bdd3c5b08628b1f7
sekaininagaja <sekaininagaja@gmail.com>
2019-08-05 16:46:18 +0900

    opne 파일 모드 변경

--- get_ip.py
+++ get_ip.py
@@ -7,13 +7,11 @@ def main():
     try:
         response = urllib.request.urlopen(url=settings.IP_GET_URL, timeout=5)
         content = response.read().decode('utf-8')
-        f = open(settings.IP_ADDR_FILE, 'r')
+        f = open(settings.IP_ADDR_FILE, 'w+t')
         ip = f.readline()
-        f.close()
         if ip != content:
-            f = open(settings.IP_ADDR_FILE, 'w')
             f.write(content)
-            f.close()
+        f.close()
     except urllib.error as e:
         print(type(e))
 
