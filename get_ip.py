2f5e0074f500be08acd37cb0be7a6feba60d5019
sekaininagaja <sekaininagaja@gmail.com>
2019-08-05 18:46:17 +0900

    open mode 수정

--- get_ip.py
+++ get_ip.py
@@ -7,10 +7,11 @@ def main():
     try:
         response = urllib.request.urlopen(url=settings.IP_GET_URL, timeout=5)
         content = response.read().decode('utf-8')
-        with open(settings.IP_ADDR_FILE, mode='w+t', encoding='utf-8') as f:
+        with open(settings.IP_ADDR_FILE, mode='r+t') as f:
             ip = f.readline()
             if ip != content:
-                f.write(content)
+                f.seek(0)
+                f.writelines(content)
                 ip_put_url = 'http://dyn.value-domain.com/cgi-bin/dyn.fcg?d=%s&p=%s&h=%s&i=%s' % (
                 settings.DOMAIN, settings.PASSWORD, settings.HOST, content)
                 print(ip_put_url)
