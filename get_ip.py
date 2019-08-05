b917b29713c8554186ccee2a41dc54baad6aaf2b
sekaininagaja <sekaininagaja@gmail.com>
2019-08-05 16:34:47 +0900

    아이피가 변경 되면 새로 저장해서 타임스템프를 변경

--- get_ip.py
+++ get_ip.py
@@ -1,13 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import urllib.request, urllib.error
 import settings
 
 def main():
     try:
         response = urllib.request.urlopen(url=settings.IP_GET_URL, timeout=5)
-        content = response.read()
-        f = open(settings.IP_ADDR_FILE, 'w')
-        f.write(content.decode('utf-8'))
+        content = response.read().decode('utf-8')
+        f = open(settings.IP_ADDR_FILE, 'r')
+        ip = f.readline()
         f.close()
+        if ip != content:
+            f = open(settings.IP_ADDR_FILE, 'w')
+            f.write(content)
+            f.close()
     except urllib.error as e:
         print(type(e))
 
