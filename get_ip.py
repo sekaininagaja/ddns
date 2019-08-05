926d81a4e1e0578b55d2f6811827069bb30b6b97
sekaininagaja <sekaininagaja@gmail.com>
2019-08-05 17:46:50 +0900

    with를 이용

--- get_ip.py
+++ get_ip.py
@@ -7,11 +7,10 @@ def main():
     try:
         response = urllib.request.urlopen(url=settings.IP_GET_URL, timeout=5)
         content = response.read().decode('utf-8')
-        f = open(settings.IP_ADDR_FILE, 'w+t')
-        ip = f.readline()
-        if ip != content:
-            f.write(content)
-        f.close()
+        with open(settings.IP_ADDR_FILE, mode='w+t', encoding='utf-8') as f:
+            ip = f.readline()
+            if ip != content:
+                f.write(content)
     except urllib.error as e:
         print(type(e))
 
